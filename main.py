fda6bf87b61c5866200b06ea6b099f04b275d07a
Axel M <axelmunoa@gmail.com>
2022-09-20 17:56:09 -0300

    ejercicio 2

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #Ejercicios 2
+import math
+
 
 #Ejercicio1
 
 def area_rectangulo(altura, base):
     return altura * base
+
+
+#Ejercicios2
+
+def area_circulo(radio):
+    return math.pi * (radio * radio)
